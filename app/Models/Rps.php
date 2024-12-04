c3552da8d04b009210c1e28524140079401122fe
Adriano dos Santos Freide <adriano.freide@meisters.solutions>
2024-12-04 20:28:04 -0300

    E60-12 CREATE TABLE RPS
    Signed-off-by: Adriano dos Santos Freide <adriano.freide@meisters.solutions>

---
+++ app/Models/Rps.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rps extends Model
+{
+    /** @use HasFactory<\Database\Factories\RpsFactory> */
+    use HasFactory;
+}
