65078eac521ea9c3cdcc4e375770daf1e9a61c62
Adriano dos Santos Freide <adriano.freide@meisters.solutions>
2024-12-04 20:40:48 -0300

    E60-12 CREATE TABLE FRIENDSHIP
    Signed-off-by: Adriano dos Santos Freide <adriano.freide@meisters.solutions>

---
+++ app/Models/Friendships.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Friendships extends Model
+{
+    /** @use HasFactory<\Database\Factories\FriendshipsFactory> */
+    use HasFactory;
+}
