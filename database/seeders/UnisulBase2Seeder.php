2ee8afbda1a2ff35bf661278dce70cb3e544c180
Adriano dos Santos Freide <adriano.freide@meisters.solutions>
2024-12-07 18:27:36 -0300

    DATABASE UNISUL BASES 2
    Signed-off-by: Adriano dos Santos Freide <adriano.freide@meisters.solutions>

---
+++ database/seeders/UnisulBase2Seeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UnisulBase2Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $oldTable = DB::table('base_unisul_2')->get();
+
+        $branchesData = $oldTable->map(function ($row) {
+            return [
+                'id' => $row->id,
+                'name' => $row->nome,
+                'email' => $row->email,
+                'cpf' => preg_replace('/\D/', '',$row->cpf),
+                'phone' => preg_replace('/\D/', '',$row->telefone),
+                'city' => $row->cidade,
+                'state' => $row->uf,
+                'school' => $row->escola,
+                'university' => $row->instituicao,
+                'campus' => $row->campus,
+                'degree' => $row->grau,
+                'referral' => $row->referral,
+                'password' => Hash::make($row->senha),
+                'created_at' => $row->data,
+            ];
+        })->toArray();
+
+        DB::table('unisul_base2s')->insert($branchesData);
+    }
+}
