c1ced71dbf9e5c62fb5d1b978a2829d4fa7c9294
Adriano dos Santos Freide <adriano.freide@meisters.solutions>
2024-12-07 18:33:20 -0300

    DATABASE UNISUL BASE 3
    Signed-off-by: Adriano dos Santos Freide <adriano.freide@meisters.solutions>

---
+++ database/seeders/UnisulBase3Seeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UnisulBase3Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $oldTable = DB::table('base_unisul_3')->get();
+
+        $data = $oldTable->map(function ($row) {
+            return [
+                'id' => $row->id,
+                'name' => $row->nome,
+                'email' => $row->email,
+                'cpf' => preg_replace('/\D/', '',$row->cpf),
+                'phone' => preg_replace('/\D/', '',$row->telefone),
+                'city' => $row->cidade,
+                'state' => $row->uf,
+                'school' => $row->escola,
+                'university' => $row->instituicao,
+                'campus' => $row->campus,
+                'degree' => $row->grau,
+                'referral' => $row->referral,
+                'password' => Hash::make($row->senha),
+                'created_at' => $row->data,
+            ];
+        })->toArray();
+
+        DB::table('unisul_base3s')->insert($data);
+    }
+}
