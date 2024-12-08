f1d29cd736541aa9f84169a6d5964c6f6a21c130
Adriano dos Santos Freide <adriano.freide@meisters.solutions>
2024-12-07 22:54:07 -0300

    DATABASE COMMETNS
    Signed-off-by: Adriano dos Santos Freide <adriano.freide@meisters.solutions>

--- database/migrations/2024_12_08_010218_create_comments_table.php
+++ database/migrations/2024_12_08_010218_create_comments_table.php
@@ -13,7 +13,24 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->longText('comment');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('parent_comment_id')->nullable();
+            $table->unsignedBigInteger('approved_by_user_id')->nullable();
+            $table->unsignedBigInteger('room_id');
+            $table->timestamp('approved_at')->nullable();
+
             $table->timestamps();
+
+            $table->index('customer_id');
+            $table->index('parent_comment_id');
+            $table->index('approved_by_user_id');
+            $table->index('room_id');
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('set null');
+            $table->foreign('parent_comment_id')->references('id')->on('comments')->onDelete('set null');
+            $table->foreign('approved_by_user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('set null');
         });
     }
 
