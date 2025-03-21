eb4f241f1960af469997c2c38078b2bba568cf48
Danilo Colasso <danilo.colasso@gmail.com>
2025-03-20 22:00:37 -0300

    feat: add coupon resource response

--- app/Http/Resources/CouponResource.php
+++ app/Http/Resources/CouponResource.php
@@ -15,7 +15,28 @@ class CouponResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            // TODO: add fields
+            'id' => $this->id,
+            'code' => $this->code,
+            'discount' => $this->discount,
+            'branches' => $this->branches()->pluck('name'),
+            'usages' => $this->customers->count(),
+            'usage_type' => $this->usage_type,
+            'quantity' => $this->quantity,
+            'valid_until' => $this->valid_until,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time,
+            'partner_name' => $this->partner_name,
+            'valid_days' => [
+                'sunday' => $this->is_valid_sunday,
+                'monday' => $this->is_valid_monday,
+                'tuesday' => $this->is_valid_tuesday,
+                'wednesday' => $this->is_valid_wednesday,
+                'thursday' => $this->is_valid_thursday,
+                'friday' => $this->is_valid_friday,
+                'saturday' => $this->is_valid_saturday,
+            ],
+            'booking_start_date' => $this->booking_start_date,
+            'booking_end_date' => $this->booking_end_date,
         ];
     }
 }
